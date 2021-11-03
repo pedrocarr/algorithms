75cf4c784505e9f3cd0ada38c3c254d2b27501e4
Pedro Carvalho <eng.pedro.amb@gmail.com>
2021-11-03 16:44:30 -0300

    chore: add new insertionsort resolution

--- insertion-sort/insertion-sort.js
+++ insertion-sort/insertion-sort.js
@@ -1,16 +1,12 @@
 const insertionSort = (nums) => {
-    for (let i = 1; i < nums.length; i++) {
-        let numberToInsert = nums[i]; // the numberToInsert number we're looking to insert
-        let j; // the inner counter
-    
-        // loop from the right to the left
-        for (j = i - 1; nums[j] > numberToInsert && j >= 0; j--) {
-          // move numbers to the right until we find where to insert
-          nums[j + 1] = nums[j];
-        }
-    
-        // do the insertion
-        nums[j + 1] = numberToInsert;
-      }
-      return nums;
-};
+  for (let i = 1; i < nums.length; i++) {
+    let numberToInsert = nums[i];
+    let j;
+    for (j = i - 1; nums[j] < numberToInsert && j >= 0; j--) {
+      nums[j + 1] = nums[j]
+    }
+    nums[j + 1] = numberToInsert;
+  }
+  return nums;
+
+}
