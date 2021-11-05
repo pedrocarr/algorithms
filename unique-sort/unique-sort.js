c636573432ff5ef22e10074596e8474bc890784f
Pedro Carvalho <eng.pedro.amb@gmail.com>
2021-11-05 15:28:36 -0300

    chore: add unique-sort big 0 file

--- unique-sort/unique-sort.js
+++ unique-sort/unique-sort.js
@@ -1,4 +1,4 @@
 // task: transform this simple sorting algorithm inyo a unique sort
 // It should not return any duplicate values in the sorted array.
 
-//input: [1, 5, 2, 1]
+//input: [1, 5, 2, 1] => output: [1,2,5]
