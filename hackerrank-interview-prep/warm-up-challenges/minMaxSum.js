3d34fe07be55c746d56761716bf5478fa229d877
pedrocarr <eng.pedro.amb@gmail.com>
2022-05-16 21:49:01 -0300

    chore: add minMaxValue solution

---
+++ hackerrank-interview-prep/warm-up-challenges/minMaxSum.js
@@ -0,0 +1,8 @@
+function miniMaxSum(arr) {
+  let sum = arr.reduce((a, b) => a + b);
+  let maxVal = Math.max(...arr);
+  let minVal = Math.min(...arr);
+  console.log((sum - maxVal) + ' ' + (sum - minVal));
+}
+
+miniMaxSum([1, 2, 3, 4, 5]);
