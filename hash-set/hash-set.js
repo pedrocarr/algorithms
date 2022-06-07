fba88181e728d356e73b3a9e26d131932a3274a1
pedrocarr <eng.pedro.amb@gmail.com>
2022-06-07 20:38:39 -0300

    chore: hash mapm search insert hash set

---
+++ hash-set/hash-set.js
@@ -0,0 +1,20 @@
+class MyHashSet {
+  constructor() {
+      this.data = []
+  }
+  
+  contains(key){
+      if (this.data.includes(key)) {
+          return true
+      }
+      else return false
+  }
+  add(key) {
+      if (!this.data.includes(key)) {
+          this.data.push(key)
+      } else return;
+  }
+  remove(key){
+      this.data = this.data.filter(data => data !== key)
+  }
+}
