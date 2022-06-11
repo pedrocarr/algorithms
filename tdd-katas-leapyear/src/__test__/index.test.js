1f54355ad913178d177c14cd83d178f1a625c8f8
pedrocarr <eng.pedro.amb@gmail.com>
2022-06-11 19:55:32 -0300

    add lapyear algorithm

---
+++ tdd-katas-leapyear/src/__test__/index.test.js
@@ -0,0 +1,23 @@
+const isLeapYear = require('../index')
+
+
+describe('checkIfIsLeapYear', () => {
+  it('should return false if year is not divisible by 4', () => {
+    expect(isLeapYear.checkIfIsLeapYear(1997)).toEqual(false)
+  });
+
+  it('should return true if year is divisible by 4', () => {
+    expect(isLeapYear.checkIfIsLeapYear(1996)).toEqual(true)
+  });
+
+  it('should return false if year is divisible by 4, by 100, not by 400', () => {
+    expect(isLeapYear.checkIfIsLeapYear(1800)).toEqual(false)
+  });
+
+  it('should return true if year is divisible by 400', () => {
+    expect(isLeapYear.checkIfIsLeapYear(1600)).toEqual(true)
+  });
+
+
+  
+});
--- tdd-katas-leapyear/src/index.js
+++ tdd-katas-leapyear/src/index.js
@@ -1,2 +1,7 @@
+const checkIfIsLeapYear = (year) => {
+return (year % 100 === 0) ? (year % 400 === 0) : (year % 4 === 0)
+  
+}
 
 
+module.exports.checkIfIsLeapYear = checkIfIsLeapYear;
