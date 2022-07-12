a69d4f1d81f8e74c4ef46395f9fe7d5e91776594
pedrocarr <eng.pedro.amb@gmail.com>
2022-07-12 15:33:05 -0300

    chore: test environment done

--- test-environment/src/__test__/index.test.js
+++ test-environment/src/__test__/index.test.js
@@ -1,46 +1,11 @@
-const romanNumeralsGenerator = (number) => {
-  let roman = "";
-  const romanToDecimal = {
-    "X": 10,
-    "V": 5,
-    "I": 1,
-  }
- 
-
-  for (key in romanToDecimal) {
-    for(i = 0; i < number; i++)
-    if (number >= romanToDecimal[key]) {
-      roman += Object.keys(romanToDecimal).map(key => romanToDecimal[key][i])
-      number -= romanToDecimal[key]
-    }
-
-  }
-  // if(number >= 10) {
-  //   roman += 'X';
-  //   number -= 10;
-  // }
-  // if (number >= 5) {
-  //   roman += 'V';
-  //   number -= 5;
-  // }
-  // for (let i = 0; i < number; i++) {
-  //   roman += 'I';
-  // }
-  // return roman
-}
 
 
 
-describe('romanNumeralsGenerator', () => { //ARRANGE
+describe('', () => { //ARRANGE
 
-  it('should generate a roman numeral for a given decimal number', () => { // ASSERT
+  it('', () => { // ASSERT
 
-    expect(romanNumeralsGenerator(1)).toEqual("I"); // ACT
-    expect(romanNumeralsGenerator(2)).toEqual("II");
-    expect(romanNumeralsGenerator(3)).toEqual("III");
-    expect(romanNumeralsGenerator(5)).toEqual("V");
-    expect(romanNumeralsGenerator(7)).toEqual("VII");
-    expect(romanNumeralsGenerator(10)).toEqual("X");
-    expect(romanNumeralsGenerator(18)).toEqual("XVIII");
+    expect(xpto(1)).toEqual(2); // ACT
+
   });
 });
