722c1b992cfd9ec561ad8b76fe0e6128b61bef5f
pedrocarr <eng.pedro.amb@gmail.com>
2022-07-13 09:10:56 -0300

    add test case one done

--- codurance-interview/src/__test__/index.spec.js
+++ codurance-interview/src/__test__/index.spec.js
@@ -1,35 +1,14 @@
 
 
-class Rover {
-  execute(instructions) {
-    if (instructions === "R") {
-      return "0:0:E"
-    }
-    if (instructions === "RR") {
-      return "0:0:S"
-    }
-    return "0:0:N"
-  };
-}
 
-describe('rover test', () => {
 
-  let rover;
+describe('', () => {
 
-  beforeEach(() => {
-    rover = new Rover();
-  });
 
-  it("should when no instructions are given have innitial position 0:0:N",()=>{
-    expect(rover.execute("")).toBe("0:0:N");
+
+  it("should",()=>{
+    expect(1).toBe(2);
 
   });
 
-  it("should face EAST when rotating to the right",()=>{
-    expect(rover.execute("R")).toBe("0:0:E");
-  });
-
-  it("should face SOUTH when rotating twice right",()=>{
-    expect(rover.execute("RR")).toBe("0:0:S");
-  });
 })
