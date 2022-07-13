2a1b46ad51ad25c89df1aedf121b5d9e1d9b105c
pedrocarr <eng.pedro.amb@gmail.com>
2022-07-13 13:37:22 -0300

    add test case one done

--- codurance-interview/src/__test__/index.spec.js
+++ codurance-interview/src/__test__/index.spec.js
@@ -1,14 +1,45 @@
+const romanNumeralGenerator = (decimal) => {
+  let roman = "";
+  const romanToDecimal = {
+    M: 1000,
+    CM: 900,
+    D: 500,
+    CD: 400,
+    C: 100,
+    XC: 90,
+    L: 50,
+    XL: 40,
+    X: 10,
+    IX: 9,
+    V: 5,
+    IV: 4,
+    I: 1,
+  }
+  Object.entries(romanToDecimal).forEach(([key, value]) =>{
+    while (decimal >=  value) {
+      roman += key;
+      decimal -= value;
+    }
+  })
+  return roman;
+}
 
+describe('romanNumeralGenerator', () => {
 
+  it("should convert a roman numeral for a given decimal number",()=>{
 
+    const results = {
+      "I": 1,
+      "II": 2,
+      "III": 3,
+      "IV": 4,
+      "V": 5,
+      "MMXIX": 2019,
+    }
 
-describe('', () => {
-
-
-
-  it("should",()=>{
-    expect(1).toBe(2);
-
+    Object.entries(results).forEach(([key, value]) =>{
+        expect(romanNumeralGenerator(value)).toBe(key);
+    });
   });
 
 })
