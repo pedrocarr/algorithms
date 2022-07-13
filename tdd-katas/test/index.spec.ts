77af66f349ed7a0a0265e9f1f1fb45c05376f289
pedrocarr <eng.pedro.amb@gmail.com>
2022-07-12 23:33:00 -0300

    add test environment case tests

--- tdd-katas/test/index.spec.ts
+++ tdd-katas/test/index.spec.ts
@@ -1,8 +1,9 @@
-const fizzBuzzFunction = (n) => {
+
+const fizzBuzzFunction = (n: number):string => {
   if (n % 5 === 0 && n % 3 === 0) return "FizzBuzz"
   if (n % 5 === 0) return "Buzz"
   if (n % 3 === 0) return "Fizz"
-  return n;
+  return String(n);
 }
 
 
