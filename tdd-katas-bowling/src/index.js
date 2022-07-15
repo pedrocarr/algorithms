f2ae3847190fc623e08be32d68787c252f2e5f3c
pedrocarr <eng.pedro.amb@gmail.com>
2022-07-15 20:03:39 -0300

    add reduce array to bowling kata

--- tdd-katas-bowling/src/index.js
+++ tdd-katas-bowling/src/index.js
@@ -1,13 +1,10 @@
 
 
 const calcScore = (rolls) => {
-  let score = 0;
-  for (let i = 0; i < rolls.legnth; i++) {
-    const roll = rolls[i];
-    score += roll;
-  }
-
-  return score
+  const rollsSum = rolls.reduce((a, b) => a + b);
+  
+  return rollsSum;
+  
 }
 
 
