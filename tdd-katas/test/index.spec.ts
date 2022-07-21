2a55dfa3acef012dd92f93cc586a66c6ab21c06e
pedrocarr <pedro.carvalho@ab-inbev.com>
2022-07-20 21:05:48 -0300

    take the test off

--- tdd-katas/test/index.spec.ts
+++ tdd-katas/test/index.spec.ts
@@ -7,29 +7,3 @@ const fizzBuzzFunction = (n: number):string => {
 }
 
 
-describe('fizzBuzzFunction', () => {
-
-  it('should return n if n is not divisible by 5 and is not divisible by 3', () => {
-
-    expect(fizzBuzzFunction(1)).toEqual(1)
-    expect(fizzBuzzFunction(2)).toEqual(2)
-  });
-
-  it('should return "Fizz" if n is divisible by 3', () => {
-    
-    expect(fizzBuzzFunction(3)).toEqual("Fizz")
-    expect(fizzBuzzFunction(6)).toEqual("Fizz")
-  });
-
-  it('should return "Buzz" if n is divisible by 5', () => {
-    
-    expect(fizzBuzzFunction(5)).toEqual("Buzz")
-    expect(fizzBuzzFunction(10)).toEqual("Buzz")
-  });
-
-  it('should return "FizzBuzz" if n is divisible by 5 and by 3', () => {
-    
-    expect(fizzBuzzFunction(15)).toEqual("FizzBuzz")
-    expect(fizzBuzzFunction(30)).toEqual("FizzBuzz")
-  });
-});
