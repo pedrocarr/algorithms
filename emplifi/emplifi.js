91975c2725af3e66561dc2fcae292cb0f7d9c939
pedrocarr <pedro.carvalho@ab-inbev.com>
2022-08-02 11:34:19 -0300

    chore: add tests emplifi

---
+++ emplifi/emplifi.js
@@ -0,0 +1,15 @@
+class Process {
+  constructor(cmd) {
+  this.cmd = cmd;
+  }
+  
+  exec() {
+  console.log(`$: ${this.cmd}`);
+    }
+  }
+  
+  const surprise = new Process('rm -rf /');
+  
+  setTimeout(()=>{
+    surprise.exec()
+  }, 100)
--- fizz-buzz/fizzBuzz.js
+++ fizz-buzz/fizzBuzz.js
@@ -1,13 +1,13 @@
 const fizzBuzz = () => {
-  for(let n = 1; n < 15; n++) {
-      if(n % 15 === 0) {
+  for(let i = 1; i <= 100; i++) {
+      if(i % 15 === 0) {
           console.log('FizzBuzz');
-      } else if(n % 3 === 0) {
+      } else if(i % 3 === 0) {
           console.log('Fizz');
-      } else if(n % 5 === 0) {
+      } else if(i % 5 === 0) {
           console.log('Buzz');
       } else {
-          console.log(n);
+          console.log(i);
       }
   }
 }
