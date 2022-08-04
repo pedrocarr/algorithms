3764bfb60c4058d0cae29db49254b41d8956b5ef
pedrocarr <eng.pedro.amb@gmail.com>
2022-08-04 08:27:36 -0300

    feat: browser

--- emplifi/emplifi.js
+++ emplifi/emplifi.js
@@ -1,6 +1,7 @@
 class Process {
   constructor(cmd) {
   this.cmd = cmd;
+  this.exec = this.exec.bind(this);
   }
   
   exec() {
@@ -11,7 +12,5 @@ class Process {
 
 
   
- setTimeout(()=>{
-  surprise.exec()
- }, 10);
+ setTimeout(surprise.exec, 10);
   
