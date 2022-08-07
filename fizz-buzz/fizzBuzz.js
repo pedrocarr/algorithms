5849e5ec72001addb5efc990346a21ea3a287141
pedrocarr <eng.pedro.amb@gmail.com>
2022-08-07 17:00:10 -0300

    add fizzBuzz for and while

--- fizz-buzz/fizzBuzz.js
+++ fizz-buzz/fizzBuzz.js
@@ -1,7 +1,7 @@
 console.time('fizzBuzz')
-function fizzBuzz() {
+function fizzBuzz(n) {
     let i = 1;
-  while(i <= 100) {
+  while(i <= n) {
       if(i % 15 === 0) {
           console.log('FizzBuzz');
           i++;
@@ -16,30 +16,23 @@ function fizzBuzz() {
           i++;
       }
   }
-// let s = "";
-// let c3 = 0, c5 = 0;
-// for (let i = 1; i <= 100; i++) {
-//     c3++;
-//     c5++;
-//     if (c3 == 3) {
-//         s += "fizz";
-//         c3 = 0;
-//     }
-//     if (c5 == 5) {
-//         s += "buzz";
-//         c5 = 0;
-//     }
-//     if (s.length == 0)
-//         console.log(i);
-//     else
-//         console.log(s);
-//     s = "";
-// }
 }
 console.timeEnd('fizzBuzz');
 
-fizzBuzz();
+fizzBuzz(100);
 
+console.time('fizzBuzzFor');
+const fizzBuzzFor = (n) => {
+    for(let i = 0; i <= n; i++) {
+        if (i % 15 === 0) console.log('FizzBuzz');
+        else if(i % 3 === 0) console.log('Fizz');
+        else if(i % 5 === 0) console.log('Buzz');
+        else console.log(i);
+    }
+}
+console.timeEnd('fizzBuzzFor');
+
+fizzBuzzFor(100);
 // loop 100 times (i)
 
 // if i div 3 & 5: print "FizzBuzz"
