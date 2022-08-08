974b69b0c767d08a7f0be889b62bfae9f2293d4d
pedrocarr <eng.pedro.amb@gmail.com>
2022-08-07 21:31:02 -0300

    bind method

--- emplifi/emplifi.js
+++ emplifi/emplifi.js
@@ -1,7 +1,7 @@
 class Process {
   constructor(cmd) {
   this.cmd = cmd;
-  this.exec = this.exec.bind(this);
+  // this.exec = this.exec.bind(this);
   }
   
   exec() {
@@ -10,7 +10,9 @@ class Process {
   }
 const surprise = new Process('rm -rf /');
 
-setTimeout(surprise.exec, 2000);
+setTimeout(surprise.exec.bind(surprise), 1000);
+
+setTimeout(()=>{surprise.exec()}, 1000);
 
 console.log('Here');
   
