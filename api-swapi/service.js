b00dfd2b25d29e6fdeedb159e9b622412b95095b
Pedro Carvalho <eng.pedro.amb@gmail.com>
2024-06-15 11:18:41 +0200

    using nock to mock the request

--- api-swapi/service.js
+++ api-swapi/service.js
@@ -7,6 +7,8 @@ async function getPeople(name) {
   const url = `https://swapi.dev/api/people/?search=${name}`
   const result = await get(url)
 
+  console.log(result.data)
+
   const nameAndMass = result.data.results.map(mapPeople)
 
   return nameAndMass
--- api-swapi/test.js
+++ api-swapi/test.js
@@ -1,8 +1,39 @@
 const assert = require('assert');
 const { getPeople } = require('./service')
+const nock = require('nock')
 
 
 describe('Star Wars Test', () => {
+  beforeEach(() => {
+    const response = {
+      count: 1,
+      next: null,
+      previous: null,
+      results: [
+        {
+          name: 'R2-D2',
+          height: '96',
+          mass: '32',
+          hair_color: 'n/a',
+          skin_color: 'white, blue',
+          eye_color: 'red',
+          birth_year: '33BBY',
+          gender: 'n/a',
+          homeworld: 'https://swapi.dev/api/planets/8/',
+          vehicles: [],
+          starships: [],
+          created: '2014-12-10T15:11:50.376000Z',
+          edited: '2014-12-20T21:17:50.311000Z',
+          url: 'https://swapi.dev/api/people/3/'
+        }
+      ]
+    }
+
+    nock('https://swapi.dev/api/people')
+      .get('/?search=r2-d2')
+      .reply(200, response)
+  });
+
     it('should return the correct mapped format', async () => {
         const expected = [{
             name: 'R2-D2',
