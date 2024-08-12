71ebd6a3ddd01d4f34d104da62ff546806cd69f0
Pedro Carvalho <pedro.carvalho@emplifi.io>
2024-08-12 23:11:11 +0200

    debugger examples

---
+++ python-course/list3/pm.py
@@ -0,0 +1,7 @@
+import pdb
+def f(x):
+    print(1 / x)
+
+f(0)
+
+pdb.pm()
--- python-course/list3/while.py
+++ python-course/list3/while.py
@@ -1,9 +1,9 @@
 
 
-# potencia = 0
+potencia = 0
 
 
-# while potencia <= 10:
-#     breakpoint()
-#     print(2 ** potencia)
-#     potencia = potencia + 1
+while potencia <= 10:
+    breakpoint()
+    print(2 ** potencia)
+    potencia = potencia + 1
