d69891c55f0a08e8caef8d1b19f7d3a019da1a8d
Pedro Carvalho <eng.pedro.amb@gmail.com>
2025-02-09 11:49:06 +0100

    merge two arrays deepnote

--- letcode/array-string/mergeSortedArray.js
+++ letcode/array-string/mergeSortedArray.js
@@ -13,3 +13,26 @@ function merge(nums1, m, nums2, n) {
 console.log(merge([1,2,3,0,0,0], 3, [2,5,6], 3));
 
 // output: [1,2,2,3,5,6]
+
+
+const array1 = ["One", "Three", "Five"]
+const array2 = ["Two", "Four", "Six", "Seven", "Eight", "Nine"]
+
+
+
+function mergeArrays(arr1, arr2) {
+
+  const accumulator = []
+
+  const largestArray = Math.max(arr1.length, arr2.length)
+
+  for (let i = 0; i < largestArray; i++) {
+    accumulator.push(arr1[i])
+    accumulator.push(arr2[i])
+  }
+  return accumulator.filter(Boolean)
+}
+
+const result = mergeArrays(array1, array2)
+
+console.log(result)
