90b394d434ef6672750dc4db0f855d0187fecfd0
Pedro Carvalho <eng.pedro.amb@gmail.com>
2025-02-23 10:04:11 +0100

    test deepnote

--- deepnote-test/src/App.tsx
+++ deepnote-test/src/App.tsx
@@ -1,24 +1,68 @@
 import { useState } from "react";
-import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 
-import Demo from "./Demo";
-
-const queryClient = new QueryClient({
-  defaultOptions: {
-    queries: {
-      refetchOnWindowFocus: false,
-    },
-  },
-});
 
 function App() {
-  const [showDemo, setShowDemo] = useState(true);
+
+  const [blocks, setBlocks] = useState([{ id: "A1", code: "", output: ""}])
+  const [variables, setVariables] = useState({})
+
+  const addBlock = () => {
+    const newId = `A${blocks.length + 1}`
+    setBlocks([...blocks, {id: newId, code: "", output: ""}])
+  }
+
+  const handleCodeChange = (id: string, newCode: string) => {
+    setBlocks(blocks.map(block => block.id === id ? { ...block, code: newCode} : block))
+  }
+
+  const runCode = (id: string) => {
+    setBlocks(blocks.map(block => {
+      if (block.id === id) {
+        try {
+          const result = new Function("vars", `"use strict"; return (${block.code})`)(variables)
+
+          const updatedVariables = { ...variables, [id]: result}
+
+          setVariables(updatedVariables)
+          return { ...block, output: String(result)}
+        } catch (error: any) {
+          return { ...block, output: `Error: ${error.message}`}
+        }
+      }
+      return block
+    }))
+
+  }
+
 
   return (
-    <QueryClientProvider client={queryClient}>
-      <button onClick={() => setShowDemo(!showDemo)}>Toggle Demo</button>
-      {showDemo && <Demo />}
-    </QueryClientProvider>
+    <div className="flex flex-col items-center justify-center min-h-screen gap-4">
+      {blocks.map((block) => (
+      <div key={block.id} className="flex flex-col items-start gap-2">
+        <span className="font-bold">{block.id}</span>
+        <textarea
+          className="border p-2 w-80 h-20"
+          placeholder="Enter some code ..."
+          value={block.code}
+          onChange={(e) => handleCodeChange(block.id, e.target.value)}
+        />
+        <button
+        onClick={()=> runCode(block.id)}
+        className="bg-gray-600 text-white px-4 py-2 rounded">
+          Run
+        </button>
+        <div className="bg-gray-300 p-2 w-80 min-h-10 border rounded">
+          {block.output || "Output will be here"}
+        </div>
+      </div>
+      ))}
+
+      <button
+        onClick={addBlock}
+        className="bg-gray-600 text-white px-4 py-2 rounded">
+        Add new block
+      </button>
+    </div>
   );
 }
 
