60b904352bf0656a506910ff88fc0977a593b0da
Pedro Carvalho <eng.pedro.amb@gmail.com>
2025-03-22 07:17:10 +0100

    add react tests

---
+++ create-hero-react/src/App2.jsx
@@ -0,0 +1,16 @@
+import { useState, useRef } from "react";
+
+
+
+function App() {
+  const [count, setCount] = useState(0);
+
+
+
+
+  return (
+      <div>Hello</div>
+  );
+}
+
+export default App;
